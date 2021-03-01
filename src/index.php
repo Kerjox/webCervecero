33625bc32254996ba6571baabef76e04ce3aff13
Kerjox <kerjoxgamer095@gmail.com>
2021-03-01 22:39:03 +0100

    Added fonts of google

--- src/index.php
+++ src/index.php
@@ -102,9 +102,7 @@ if ($id_Receta != 0) {
     <!-- Custom fonts for this template-->
     <link rel="shortcut icon" href="./img/logo-ico.ico" />
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-    rel="stylesheet">
+    <link href="css/font.css" rel="stylesheet">
     
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.css" rel="stylesheet">
@@ -684,7 +682,7 @@ if ($id_Receta != 0) {
     <script src="js/charts/bar_Chart.js"></script>
     
     <!-- MQTT conexion-->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.min.js" type="text/javascript"></script>
+    <script src="js/mqtt-lib/mqttws31.min.js" type="text/javascript"></script>
     <script src="js/globalVariables.js"></script>
     <script src="js/index.js"></script>
     <script src="js/mqtt_Conexion.js"></script>
--- src/recipes.php
+++ src/recipes.php
@@ -51,9 +51,7 @@ $mysqli->close();
     <!-- Custom fonts for this template-->
     <link rel="shortcut icon" href="./img/logo-ico.ico" />
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="css/font.css" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.css" rel="stylesheet">
@@ -432,7 +430,7 @@ $mysqli->close();
     <!-- Set global variables-->
     <script src="js/globalVariables.js"></script>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.min.js" type="text/javascript"></script>
+    <script src="js/mqtt-lib/mqttws31.min.js" type="text/javascript"></script>
     <script src="js/recipes.js"></script>
     <script src="js/mqtt_Conexion.js"></script>
 
